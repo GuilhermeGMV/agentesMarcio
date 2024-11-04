69985f2ba36f43a281f55304e06b31b5a8c3e47b
GuilhermeMV <gui141202@gmail.com>
2024-11-04 11:45:59 -0300

    segundo commit

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return '<p>Login</p>'
+    return render_template('login.html', text="Testing")
+
 
 @auth.route('/logout')
 def logout():
     return '<p>Logout</p>'
 
-@auth.route('/sign-up')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return '<p>sign-up</p>'
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='erro')
+        elif len(password1) < 7:
+            flash('Passsword must be greater than 7 characters.', category='erro')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters.', category='erro')
+        elif password1 != password2:
+            flash('Passsword dont match.', category='erro')
+        #else:
+            # add user to db
+    return render_template('sign_up.html')
